4bcf3b3df38b563212c1845013472d24ba453fa3
Tamal Saha <tamal@appscode.com>
2023-09-19 23:46:40 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- preset_cal/main.go
+++ preset_cal/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	clustermeta "kmodules.xyz/client-go/cluster"
+	uiv1alpha1 "kmodules.xyz/resource-metadata/apis/ui/v1alpha1"
 	"sort"
 
 	"github.com/pkg/errors"
@@ -92,7 +96,199 @@ func useKubebuilderClient() error {
 	return nil
 }
 
+func LoadPresetValues(kc client.Client, ref chartsapi.ChartPresetFlatRef) ([]ChartPresetValues, error) {
+	if ref.PresetName == "" {
+		return nil, errors.New("preset name is not set")
+	}
+
+	rid := &kmapi.ResourceID{
+		Group: ref.Group,
+		Name:  ref.Resource,
+		Kind:  ref.Kind,
+	}
+	rid, err := kmapi.ExtractResourceID(kc.RESTMapper(), *rid)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to detect resource ID for %#v", *rid)
+	}
+	ed, ok := resourceeditors.LoadByGVR(kc, rid.GroupVersionResource())
+	if !ok {
+		return nil, errors.Errorf("failed to detect ResourceEditor for %#v", rid.GroupVersionResource())
+	}
+
+	var variant *uiv1alpha1.VariantRef
+	for i := range ed.Spec.Variants {
+		if ed.Spec.Variants[i].Name == ref.PresetName {
+			variant = &ed.Spec.Variants[i]
+			break
+		}
+	}
+	if variant == nil {
+		return nil, errors.Errorf("No variant with name %s found for %+v", ref.PresetName, *rid)
+	}
+
+	if variant.Selector == nil {
+		return nil, nil // ERROR?
+	}
+	sel, err := metav1.LabelSelectorAsSelector(variant.Selector)
+	if err != nil {
+		return nil, err
+	}
+
+	if ref.Namespace == "" {
+		return bundleClusterChartPresets(kc, sel, nil)
+	}
+
+	values, err := bundleChartPresets(kc, ref.Namespace, sel, nil)
+	if err != nil {
+		return nil, err
+	}
+	knownPresets := map[string]bool{} // true => cp, false => ccp
+	for _, v := range values {
+		knownPresets[v.Source.Ref.Name] = true
+	}
+
+	if clustermeta.IsRancherManaged(kc.RESTMapper()) {
+		var ns core.Namespace
+		err = kc.Get(context.TODO(), client.ObjectKey{Name: ref.Namespace}, &ns)
+		if err != nil {
+			return nil, err
+		}
+		projectId, found := ns.Labels[clustermeta.LabelKeyRancherProjectId]
+		if !found {
+			// NS not in a project. So, just add the extra CCPs
+			ccps, err := bundleClusterChartPresets(kc, sel, knownPresets)
+			if err != nil {
+				return nil, err
+			}
+			values = append(ccps, values...)
+			return values, nil
+		}
+
+		var nsList core.NamespaceList
+		err := kc.List(context.TODO(), &nsList, client.MatchingLabels{
+			clustermeta.LabelKeyRancherProjectId: projectId,
+		})
+		if err != nil {
+			return nil, err
+		}
+		namespaces := nsList.Items
+		sort.Slice(namespaces, func(i, j int) bool {
+			return namespaces[i].Name < namespaces[j].Name
+		})
+
+		projectPresets := map[string]bool{} // true => cp, false => ccp
+		for i := len(namespaces) - 1; i >= 0; i-- {
+			if namespaces[i].Name == ref.Namespace {
+				continue
+			}
+
+			nsPresets, err := bundleChartPresets(kc, namespaces[i].Name, sel, knownPresets)
+			if err != nil {
+				return nil, err
+			}
+			for _, v := range nsPresets {
+				projectPresets[v.Source.Ref.Name] = true
+			}
+			values = append(nsPresets, values...)
+		}
+		// mark project presets as known
+		for k, v := range projectPresets {
+			knownPresets[k] = v
+		}
+
+		ccps, err := bundleClusterChartPresets(kc, sel, knownPresets)
+		if err != nil {
+			return nil, err
+		}
+		values = append(ccps, values...)
+	} else {
+		ccps, err := bundleClusterChartPresets(kc, sel, knownPresets)
+		if err != nil {
+			return nil, err
+		}
+		values = append(ccps, values...)
+	}
+
+	return values, nil
+}
+
+func bundleChartPresets(kc client.Client, ns string, sel labels.Selector, knownPresets map[string]bool) ([]ChartPresetValues, error) {
+	var list chartsapi.ChartPresetList
+	err := kc.List(context.TODO(), &list, client.InNamespace(ns), client.MatchingLabelsSelector{Selector: sel})
+	if err != nil {
+		return nil, err
+	}
+	cps := list.Items
+	sort.Slice(cps, func(i, j int) bool {
+		return cps[i].Name < cps[j].Name
+	})
+
+	values := make([]ChartPresetValues, 0, len(cps))
+	for _, cp := range cps {
+		if _, exists := knownPresets[cp.Name]; exists {
+			continue
+		}
+
+		values = append(values, ChartPresetValues{
+			Source: rsapi.SourceLocator{
+				Resource: kmapi.ResourceID{
+					Group:   chartsapi.GroupVersion.Group,
+					Version: chartsapi.GroupVersion.Version,
+					Kind:    chartsapi.ResourceKindChartPreset,
+				},
+				Ref: kmapi.ObjectReference{
+					Namespace: cp.Namespace,
+					Name:      cp.Namespace,
+				},
+			},
+			Values: cp.Spec.Values,
+		})
+	}
+	return values, err
+}
+
+func bundleClusterChartPresets(kc client.Client, sel labels.Selector, knownPresets map[string]bool) ([]ChartPresetValues, error) {
+	var list chartsapi.ClusterChartPresetList
+	err := kc.List(context.TODO(), &list, client.MatchingLabelsSelector{Selector: sel})
+	if err != nil {
+		return nil, err
+	}
+
+	ccps := list.Items
+	sort.Slice(ccps, func(i, j int) bool {
+		return ccps[i].Name < ccps[j].Name
+	})
+
+	values := make([]ChartPresetValues, 0, len(ccps))
+	for _, ccp := range ccps {
+		if _, exists := knownPresets[ccp.Name]; exists {
+			continue
+		}
+
+		values = append(values, ChartPresetValues{
+			Source: rsapi.SourceLocator{
+				Resource: kmapi.ResourceID{
+					Group:   chartsapi.GroupVersion.Group,
+					Version: chartsapi.GroupVersion.Version,
+					Kind:    chartsapi.ResourceKindClusterChartPreset,
+				},
+				Ref: kmapi.ObjectReference{
+					Namespace: ccp.Namespace,
+					Name:      ccp.Namespace,
+				},
+			},
+			Values: ccp.Spec.Values,
+		})
+	}
+	return values, nil
+}
+
+// unused
 func MergePresetValues(kc client.Client, ref chartsapi.ChartPresetFlatRef) ([]ChartPresetValues, error) {
+	if ref.Namespace != "" {
+		return nil, errors.New("Only call when namespace is not known")
+	}
+
 	var values []ChartPresetValues
 
 	if ref.PresetName != "" {

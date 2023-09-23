887725af00da953a77a1b672ceae7137a0da7818
Tamal Saha <tamal@appscode.com>
2023-09-22 17:34:27 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- federate/main.go
+++ federate/main.go
@@ -4,17 +4,14 @@ import (
 	"context"
 	"fmt"
 	"github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"reflect"
-	"sort"
-	"strings"
-
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog/v2"
@@ -28,6 +25,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sort"
 )
 
 func NewClient() (*rest.Config, versioned.Interface, client.Client, error) {
@@ -111,6 +109,25 @@ func Reconcile(ctx context.Context, kc client.Client, req ctrl.Request) (ctrl.Re
 		return ctrl.Result{}, nil
 	}
 
+	// services
+	srcServices := map[client.ObjectKey]core.Service{}
+	svcSel, err := metav1.LabelSelectorAsSelector(&svcMon.Spec.Selector)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	for _, ns := range svcMon.Spec.NamespaceSelector.MatchNames {
+		var svcList core.ServiceList
+		err = kc.List(context.TODO(), &svcList, client.InNamespace(ns), client.MatchingLabelsSelector{
+			Selector: svcSel,
+		})
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		for _, svc := range svcList.Items {
+			srcServices[client.ObjectKeyFromObject(&svc)] = svc
+		}
+	}
+
 	// secret
 	var srcSecrets []core.Secret
 	for i := range svcMon.Spec.Endpoints {
@@ -157,14 +174,30 @@ func Reconcile(ctx context.Context, kc client.Client, req ctrl.Request) (ctrl.Re
 				errList = append(errList, err)
 			}
 		} else {
-			for _, srcSecret := range srcSecrets {
-				if err := copySecret(kc, &srcSecret, prom.Namespace); err != nil {
+			targetSvcMon, err := copyServiceMonitor(kc, prom, &svcMon)
+			if err != nil {
+				errList = append(errList, err)
+			}
+
+			for _, srcSvc := range srcServices {
+				if err := copyService(kc, &srcSvc, targetSvcMon); err != nil {
 					errList = append(errList, err)
+				}
+				var srcEP core.Endpoints
+				err = kc.Get(context.TODO(), client.ObjectKeyFromObject(&srcSvc), &srcEP)
+				if err != nil {
+					errList = append(errList, err)
+				} else {
+					if err := copyEndpoints(kc, &srcSvc, &srcEP, targetSvcMon); err != nil {
+						errList = append(errList, err)
+					}
 				}
 			}
 
-			if err := copyServiceMonitor(kc, prom, &svcMon); err != nil {
-				errList = append(errList, err)
+			for _, srcSecret := range srcSecrets {
+				if err := copySecret(kc, &srcSecret, targetSvcMon); err != nil {
+					errList = append(errList, err)
+				}
 			}
 		}
 	}
@@ -197,10 +230,10 @@ func updateServiceMonitorLabels(kc client.Client, prom *monitoringv1.Prometheus,
 	return nil
 }
 
-func copyServiceMonitor(kc client.Client, prom *monitoringv1.Prometheus, src *monitoringv1.ServiceMonitor) error {
+func copyServiceMonitor(kc client.Client, prom *monitoringv1.Prometheus, src *monitoringv1.ServiceMonitor) (*monitoringv1.ServiceMonitor, error) {
 	sel, err := metav1.LabelSelectorAsSelector(prom.Spec.ServiceMonitorNamespaceSelector)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	var nsList core.NamespaceList
@@ -208,7 +241,7 @@ func copyServiceMonitor(kc client.Client, prom *monitoringv1.Prometheus, src *mo
 		Selector: sel,
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	namespaces := make([]string, 0, len(nsList.Items))
@@ -241,37 +274,44 @@ func copyServiceMonitor(kc client.Client, prom *monitoringv1.Prometheus, src *mo
 
 		obj.Spec = *src.Spec.DeepCopy()
 
-		keepNSMetrics := monitoringv1.RelabelConfig{
-			Action:       "keep",
-			SourceLabels: []monitoringv1.LabelName{"namespace"},
-			Regex:        strings.Join(namespaces, "|"),
-		}
+		/*
+			keepNSMetrics := monitoringv1.RelabelConfig{
+				Action:       "keep",
+				SourceLabels: []monitoringv1.LabelName{"namespace"},
+				Regex:        strings.Join(namespaces, "|"),
+			}
+		*/
 
 		for i := range obj.Spec.Endpoints {
 			e := obj.Spec.Endpoints[i]
 
-			if len(e.RelabelConfigs) == 0 || !reflect.DeepEqual(keepNSMetrics, *e.RelabelConfigs[0]) {
-				e.RelabelConfigs = append([]*monitoringv1.RelabelConfig{
-					&keepNSMetrics,
-				}, e.RelabelConfigs...)
-				obj.Spec.Endpoints[i] = e
-			}
+			e.HonorLabels = true // keep original labels
+
+			/*
+				if len(e.RelabelConfigs) == 0 || !reflect.DeepEqual(keepNSMetrics, *e.RelabelConfigs[0]) {
+					e.RelabelConfigs = append([]*monitoringv1.RelabelConfig{
+						&keepNSMetrics,
+					}, e.RelabelConfigs...)
+				}
+			*/
+
+			obj.Spec.Endpoints[i] = e
 		}
 
 		return obj
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 	klog.Infof("%s ServiceMonitor %s/%s", vt, target.Namespace, target.Name)
-	return nil
+	return &target, nil
 }
 
-func copySecret(kc client.Client, src *core.Secret, targetNS string) error {
+func copySecret(kc client.Client, src *core.Secret, targetSvcMon *monitoringv1.ServiceMonitor) error {
 	target := core.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      src.Name,
-			Namespace: targetNS,
+			Namespace: targetSvcMon.Namespace,
 		},
 		Immutable:  nil,
 		Data:       nil,
@@ -282,12 +322,12 @@ func copySecret(kc client.Client, src *core.Secret, targetNS string) error {
 	vt, err := cu.CreateOrPatch(context.TODO(), kc, &target, func(in client.Object, createOp bool) client.Object {
 		obj := in.(*core.Secret)
 
-		//ref := metav1.NewControllerRef(prom, schema.GroupVersionKind{
-		//	Group:   monitoring.GroupName,
-		//	Version: monitoringv1.Version,
-		//	Kind:    "Prometheus",
-		//})
-		//obj.OwnerReferences = []metav1.OwnerReference{*ref}
+		ref := metav1.NewControllerRef(targetSvcMon, schema.GroupVersionKind{
+			Group:   monitoring.GroupName,
+			Version: monitoringv1.Version,
+			Kind:    "ServiceMonitor",
+		})
+		obj.OwnerReferences = []metav1.OwnerReference{*ref}
 
 		obj.Immutable = src.Immutable
 		obj.Data = src.Data
@@ -300,4 +340,124 @@ func copySecret(kc client.Client, src *core.Secret, targetNS string) error {
 	}
 	klog.Infof("%s Secret %s/%s", vt, target.Namespace, target.Name)
 	return nil
+}
+
+func copyService(kc client.Client, src *core.Service, targetSvcMon *monitoringv1.ServiceMonitor) error {
+	target := core.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      src.Name,
+			Namespace: targetSvcMon.Namespace,
+		},
+	}
+
+	vt, err := cu.CreateOrPatch(context.TODO(), kc, &target, func(in client.Object, createOp bool) client.Object {
+		obj := in.(*core.Service)
+
+		ref := metav1.NewControllerRef(targetSvcMon, schema.GroupVersionKind{
+			Group:   monitoring.GroupName,
+			Version: monitoringv1.Version,
+			Kind:    "ServiceMonitor",
+		})
+		obj.OwnerReferences = []metav1.OwnerReference{*ref}
+		obj.Labels = meta_util.OverwriteKeys(obj.Labels, src.Labels)
+		obj.Annotations = meta_util.OverwriteKeys(obj.Annotations, src.Annotations)
+
+		obj.Spec.Type = core.ServiceTypeClusterIP
+
+		for _, port := range src.Spec.Ports {
+			obj.Spec.Ports = UpsertServicePort(obj.Spec.Ports, port)
+		}
+		return obj
+	})
+	if err != nil {
+		return err
+	}
+	klog.Infof("%s Service %s/%s", vt, target.Namespace, target.Name)
+	return nil
+}
+
+func UpsertServicePort(ports []core.ServicePort, x core.ServicePort) []core.ServicePort {
+	for i, port := range ports {
+		if port.Name == x.Name {
+			port.Port = x.Port
+			port.TargetPort = intstr.FromInt(int(x.Port))
+			ports[i] = port
+			return ports
+		}
+	}
+	return append(ports, core.ServicePort{
+		Name:       x.Name,
+		Port:       x.Port,
+		TargetPort: intstr.FromInt(int(x.Port)),
+	})
+}
+
+func copyEndpoints(kc client.Client, srcSvc *core.Service, srcEP *core.Endpoints, targetSvcMon *monitoringv1.ServiceMonitor) error {
+	target := core.Endpoints{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      srcEP.Name,
+			Namespace: targetSvcMon.Namespace,
+		},
+	}
+
+	vt, err := cu.CreateOrPatch(context.TODO(), kc, &target, func(in client.Object, createOp bool) client.Object {
+		obj := in.(*core.Endpoints)
+
+		ref := metav1.NewControllerRef(targetSvcMon, schema.GroupVersionKind{
+			Group:   monitoring.GroupName,
+			Version: monitoringv1.Version,
+			Kind:    "ServiceMonitor",
+		})
+		obj.OwnerReferences = []metav1.OwnerReference{*ref}
+		obj.Labels = meta_util.OverwriteKeys(obj.Labels, srcEP.Labels)
+		obj.Annotations = meta_util.OverwriteKeys(obj.Annotations, srcEP.Annotations)
+
+		for i, srcSubNet := range srcEP.Subsets {
+			if i >= len(obj.Subsets) {
+				obj.Subsets = append(obj.Subsets, core.EndpointSubset{})
+			}
+
+			obj.Subsets[i].Addresses = []core.EndpointAddress{
+				{
+					IP: srcSvc.Spec.ClusterIP,
+				},
+			}
+			for _, port := range srcSubNet.Ports {
+				obj.Subsets[i].Ports = UpsertEndpointPort(obj.Subsets[i].Ports, core.EndpointPort{
+					Name: port.Name,
+					Port: ServicePortForName(srcSvc, port.Name),
+				})
+			}
+		}
+
+		return obj
+	})
+	if err != nil {
+		return err
+	}
+	klog.Infof("%s Endpoints %s/%s", vt, target.Namespace, target.Name)
+	return nil
+}
+
+func ServicePortForName(svc *core.Service, portName string) int32 {
+	for _, port := range svc.Spec.Ports {
+		if port.Name == portName {
+			return port.Port
+		}
+	}
+	panic(fmt.Errorf("service %s/%s has no port with name %s", svc.Namespace, svc.Name, portName))
+}
+
+func UpsertEndpointPort(ports []core.EndpointPort, x core.EndpointPort) []core.EndpointPort {
+	for i, port := range ports {
+		if port.Name == x.Name {
+			port.Port = x.Port
+			ports[i] = port
+			return ports
+		}
+	}
+	return append(ports, core.EndpointPort{
+		Name: x.Name,
+		Port: x.Port,
+	})
 }

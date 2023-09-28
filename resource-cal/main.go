30fb712063d6f0ad20bf41f1147024b5fad18a25
Tamal Saha <tamal@appscode.com>
2023-09-27 21:09:09 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- resource-cal/main.go
+++ resource-cal/main.go
@@ -59,7 +59,7 @@ func useKubebuilderClient() error {
 		return err
 	}
 
-	apiTypes, err := ListKinds(disc, "kubedb.com")
+	apiTypes, err := ListKinds(disc)
 	if err != nil {
 		return err
 	}
@@ -79,7 +79,7 @@ type APIType struct {
 	Versions []string
 }
 
-func ListKinds(c discovery.DiscoveryInterface, group string) (map[string]APIType, error) {
+func ListKinds(c discovery.DiscoveryInterface) (map[string]APIType, error) {
 	_, resourceList, err := c.ServerGroupsAndResources()
 
 	apiTypes := map[string]APIType{}
@@ -89,10 +89,11 @@ func ListKinds(c discovery.DiscoveryInterface, group string) (map[string]APIType
 			if err != nil {
 				return nil, err
 			}
-			if gv.Group != group {
-				continue
-			}
 			for _, resource := range resources.APIResources {
+				if strings.ContainsRune(resource.Name, '/') {
+					continue
+				}
+
 				gk := schema.GroupKind{
 					Group: gv.Group,
 					Kind:  resource.Kind,
@@ -113,82 +114,14 @@ func ListKinds(c discovery.DiscoveryInterface, group string) (map[string]APIType
 		}
 	}
 
-	/*
-		for gk, x := range apiTypes {
-			if len(x.Versions) > 1 {
-				sort.Slice(x.Versions, func(i, j int) bool {
-					return apiversion.MustCompare(x.Versions[i], x.Versions[j]) > 0
-				})
-				apiTypes[gk] = x
-			}
+	for gk, x := range apiTypes {
+		if len(x.Versions) > 1 {
+			sort.Slice(x.Versions, func(i, j int) bool {
+				return apiversion.MustCompare(x.Versions[i], x.Versions[j]) > 0
+			})
+			apiTypes[gk] = x
 		}
-
-	*/
+	}
 
 	return apiTypes, nil
-}
-
-func ListAPIVersions(c discovery.DiscoveryInterface, group, kind string) ([]string, error) {
-	_, resourceList, err := c.ServerGroupsAndResources()
-
-	out := make([]string, 0)
-	if discovery.IsGroupDiscoveryFailedError(err) || err == nil {
-		for _, resources := range resourceList {
-			gv, err := schema.ParseGroupVersion(resources.GroupVersion)
-			if err != nil {
-				return nil, err
-			}
-			if gv.Group != group {
-				continue
-			}
-			for _, resource := range resources.APIResources {
-				if resource.Kind == kind && !strings.ContainsRune(resource.Name, '/') {
-					out = append(out, gv.Version)
-				}
-			}
-		}
-	}
-	if len(out) > 1 {
-		sort.Slice(out, func(i, j int) bool {
-			return apiversion.MustCompare(out[i], out[j]) > 0
-		})
-	}
-	return out, nil
-}
-
-func ListKinds2(kc client.Client) error {
-	rs, err := kc.RESTMapper().ResourcesFor(schema.GroupVersionResource{
-		Group:    "kubedb.com",
-		Version:  "",
-		Resource: "",
-	})
-	if err != nil {
-		return err
-	}
-	fmt.Println(rs)
-
-	kinds, err := kc.RESTMapper().KindsFor(schema.GroupVersionResource{
-		Group:    "kubedb.com",
-		Version:  "",
-		Resource: "",
-	})
-	if err != nil {
-		return err
-	}
-	for _, k := range kinds {
-		fmt.Println(k)
-	}
-
-	mappings, err := kc.RESTMapper().RESTMappings(schema.GroupKind{
-		Group: "kubedb.com",
-		Kind:  "",
-	})
-	if err != nil {
-		return err
-	}
-	for _, mapping := range mappings {
-		fmt.Println(mapping.GroupVersionKind)
-	}
-
-	return nil
 }
